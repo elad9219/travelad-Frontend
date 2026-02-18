64614e67367886f3ef5f366434199cced0bdd4dc
elad9219 <elad9219@gmail.com>
2026-02-18 15:06:41 +0200

    UI: Adjusted SearchBar mobile width and updated local IP

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,16 +1,63 @@
+// class Globals {}
+
+// export class DevelopmentGlobals extends Globals {
+//     public api = {
+//         places: 'http://localhost:8080/api/places/search',
+//         flights: 'http://localhost:8080/flights',
+//         advancedFlights: 'http://localhost:8080/flights/advancedFlightSearch',
+//         hotels: 'http://localhost:8080/hotels/by-city-name',
+//         hotelOffers: 'http://localhost:8080/hotels/offers',
+//         attractions: 'http://localhost:8080/api/geoapify/places',
+//         weather: 'http://localhost:8080/weather',
+//         iataCodes: 'http://localhost:8080/iata-codes',
+//         cacheCities: 'http://localhost:8080/cache/cities', // Points to CityCacheController
+//     };
+// }
+
+// export class ProductionGlobals extends Globals {
+//     public api = {
+//         places: '/api/places/search',
+//         flights: '/flights',
+//         advancedFlights: '/flights/advancedFlightSearch',
+//         hotels: '/hotels/by-city-name',
+//         hotelOffers: '/hotels/offers',
+//         attractions: '/api/geoapify/places',
+//         weather: '/weather',
+//         iataCodes: '/iata-codes',
+//         cacheCities: '/cache/cities', // Points to CityCacheController
+//     };
+// }
+
+// const globals = process.env.NODE_ENV === 'production'
+//     ? new ProductionGlobals()
+//     : new DevelopmentGlobals();
+
+// export default globals;
+
+
+
+
+
+
+
+
+
+
 class Globals {}
 
 export class DevelopmentGlobals extends Globals {
+    private static ip = "192.168.1.19"; 
+
     public api = {
-        places: 'http://localhost:8080/api/places/search',
-        flights: 'http://localhost:8080/flights',
-        advancedFlights: 'http://localhost:8080/flights/advancedFlightSearch',
-        hotels: 'http://localhost:8080/hotels/by-city-name',
-        hotelOffers: 'http://localhost:8080/hotels/offers',
-        attractions: 'http://localhost:8080/api/geoapify/places',
-        weather: 'http://localhost:8080/weather',
-        iataCodes: 'http://localhost:8080/iata-codes',
-        cacheCities: 'http://localhost:8080/cache/cities', // Points to CityCacheController
+        places: "http://" + DevelopmentGlobals.ip + ":8080/api/places/search",
+        flights: "http://" + DevelopmentGlobals.ip + ":8080/flights",
+        advancedFlights: "http://" + DevelopmentGlobals.ip + ":8080/flights/advancedFlightSearch",
+        hotels: "http://" + DevelopmentGlobals.ip + ":8080/hotels/by-city-name",
+        hotelOffers: "http://" + DevelopmentGlobals.ip + ":8080/hotels/offers",
+        attractions: "http://" + DevelopmentGlobals.ip + ":8080/api/geoapify/places",
+        weather: "http://" + DevelopmentGlobals.ip + ":8080/weather",
+        iataCodes: "http://" + DevelopmentGlobals.ip + ":8080/iata-codes",
+        cacheCities: "http://" + DevelopmentGlobals.ip + ":8080/cache/cities",
     };
 }
 
@@ -24,7 +71,7 @@ export class ProductionGlobals extends Globals {
         attractions: '/api/geoapify/places',
         weather: '/weather',
         iataCodes: '/iata-codes',
-        cacheCities: '/cache/cities', // Points to CityCacheController
+        cacheCities: '/cache/cities',
     };
 }
 
